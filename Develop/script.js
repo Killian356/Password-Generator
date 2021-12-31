4c4877208fb3826bb1a070202cb09d08e90181c2
Killian Chamberlain <killian7195@gmail.com>
2021-12-31 17:07:29 -0600

    adding password Characteristics

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,30 @@
-// Assignment code here
-
-
 // Get references to the #generate element
+
 var generateBtn = document.querySelector("#generate");
+var passlength, cuppercase, clowercase, csymbols, cnumbers;
+function askQuestions() {
+
+  while (passlength < 8 || passlength > 128) {
+    passlength = prompt("Length must be 8-128 characters");
+  }
+  passlength= prompt("How many characters would you like your password to be?");
+  cuppercase = confirm("Would you like to use uppercase letters?");
+  clowercase = confirm("Would you like to use lowercase letters?");
+  cnumbers = confirm("would you like to use numbers?");
+  csymbols = confirm("would you like to use special characters?");
+
+  while (!(cuppercase || clowercase || cnumbers || csymbols)) {
+    alert("You must select at least one character type!");
+
+    cuppercase = confirm("Would you like to use uppercase letters?");
+    clowercase = confirm("Would you like to use lowercase letters?");
+    csymbols = confirm("would you like to use special characters?");
+    cnumbers = confirm("would you like to use numbers?");
+
+  }
+}
+
+// Generate Passwoard
 
 // Write password to the #password input
 function writePassword() {
